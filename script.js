fb521528b06227138f06f7dca50cf3abedc3e703
Praveen <waytosearch454@gmail.com>
2024-04-02 08:22:54 +0530

    Made Changes

--- script.js
+++ script.js
@@ -8,20 +8,50 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(employee => {
+    if(employee.profession=='developer'){
+      console.log(employee)
+    }
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(employee => {
+    if(employee.profession=='developer'){
+      console.log(employee);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newArr= {id:4,name:"susan",age:"20",profession:"intern"};
+  arr.push(newArr);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter(employee => employee.profession!="admin")
+  console.log(arr)
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [
+    { id: 4, name: "Praveen", age: "24", profession: "Mentor" },
+    { id: 5, name: "Prayag", age: "16", profession: "Student" },
+    { id: 6, name: "Ishika", age: "24", profession: "Doctor" }
+  ]
+  let concatenateArrays = arr.concat(newArr);
+  console.log(concatenateArrays)
 }
+
+
+PrintDeveloperbyMap();
+PrintDeveloperbyForEach();
+addData();
+removeAdmin();
+concatenateArray();
+
